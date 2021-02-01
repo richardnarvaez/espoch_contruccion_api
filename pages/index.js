38552de4bfc0c4512a70f8d83ebad51a884e7dc4
Richard Narvaez <richard.bvn997@gmail.com>
2021-02-01 12:52:06 -0500

    First commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    fetch(
+      "https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true",
+      {
+        method: "GET",
+      }
+    ).then(async (resp) => {
+      const dts = await resp.json();
+      console.log("Data:", dts);
+      setData(dts);
+    });
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,42 +26,41 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Estadisticas <a>COVID-19</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Trabajo de clases, obtener datos de un API y presentarlos.
         </p>
 
+        <p>
+          https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true
+        </p>
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {/* country: "Algeria"
+              deceased: 2891
+              historyData: "https://api.apify.com/v2/datasets/hi0DJXpcyzDwtg2Fm/items?format=json&clean=1"
+              infected: 107339
+              lastUpdatedApify: "2021-02-01T16:55:00.000Z"
+              moreData: "https://api.apify.com/v2/key-value-stores/pp4Wo2slUJ78ZnaAi/records/LATEST?disableRedirect=true"
+              recovered: 73344
+              sourceUrl: "https://www.worldometers.info/coronavirus/"
+              tested: "NA" */}
+          {data &&
+            data.map((r, i) => {
+              return (
+                <a
+                  key={i}
+                  href="https://nextjs.org/docs"
+                  className={styles.card}
+                >
+                  <h3>{r.country}</h3>
+                  <p>Infectados: {r.infected}</p>
+                  <p>Recuperados: {r.recovered}</p>
+                  <p>Fallecidos: {r.deceased}</p>
+                </a>
+              );
+            })}
         </div>
       </main>
 
@@ -56,10 +70,9 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Construcción de SOFTWARE APIs | Powered by Richard Vinueza (6745)
         </a>
       </footer>
     </div>
-  )
+  );
 }
